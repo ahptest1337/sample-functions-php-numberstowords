d9a6b2901fb2ce70f7b043459853474494e030aa
ahptest1337 <102034204+ahptest1337@users.noreply.github.com>
2022-04-27 14:55:01 +0200

    Update build.sh

--- packages/default/n2w/build.sh
+++ packages/default/n2w/build.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 echo 'start'
-for i in {1..100}; do echo -n "This is a test in loop $i "; date ; sleep 1; done
+# for i in {1..100}; do echo -n "This is a test in loop $i "; date ; sleep 1; done
 
 set -e
 
-while true; do echo '1337'; sleep 1; done
+# while true; do echo '1337'; sleep 1; done
 
 composer install
